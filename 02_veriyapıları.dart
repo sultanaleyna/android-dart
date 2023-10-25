11e8dda8fad324da9c245963ea4e55679aa1f354
sultanaleyna <ustuneraleynaa@gmail.com>
2023-10-25 16:01:22 +0300

    u

---
+++ 02_veriyapıları.dart
@@ -0,0 +1,31 @@
+void main(List<String> args) {
+  
+
+
+  //dart strongly type bir dildir, değişkenlerin türünü yazmalıyız int double ..
+  //ama dynammic tipleri de destekler
+  int a=5;
+
+  //sadece dynamic veri yapısına sahip diller :js ,pyhton tür yazmasakta olur ve değiştirebiliriz.
+  //b=6
+  //b="ahmet";
+
+
+  
+  /*
+  1)numbers types
+  int
+  double
+  2)string type
+  3)boolean types
+  4)bunun dışındaki hepsi obje ve ilk üçünü kapsar
+  dartta her şey objelerden oluşur
+  
+
+  
+
+  */
+
+
+
+}
--- 1.ders.dart
+++ 1.ders.dart
@@ -1,4 +1,7 @@
-// entry point program çalıştırıldığında ilk tetiklenecek yer
 void main(List<String> args) {
-  print("merhaba dünya ");
+  print("merhaba dünya");
+  //geliştirme ortamında yorumlanan bir dildir projeye çevrildiğinde derlenmiş bir dildir
+  // programlama dillerinin paradigmaları
+  // java multi paradigmalı bir dildir
+  
 }
